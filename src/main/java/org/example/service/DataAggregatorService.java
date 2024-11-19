189b2dad3abd13e1e1c3b2a920b56ff7077af495
Alena <19lenka87@tut.by>
2024-11-19 21:40:09 +0300

    17. Renamed NonOperatingIncomeService

--- src/main/java/org/example/service/DataAggregatorService.java
+++ src/main/java/org/example/service/DataAggregatorService.java
@@ -1,10 +1,10 @@
 package org.example.service;
 
 public class DataAggregatorService {
-    private final Non_Operating_Income_Service currencyDifferenceService;
+    private final NonOperatingIncomeService currencyDifferenceService;
     private final ForeignCurrencyAccountantTableService transactionService;
 
-    public DataAggregatorService(Non_Operating_Income_Service currencyDifferenceService,
+    public DataAggregatorService(NonOperatingIncomeService currencyDifferenceService,
                                  ForeignCurrencyAccountantTableService transactionService) {
         this.currencyDifferenceService = currencyDifferenceService;
         this.transactionService = transactionService;
